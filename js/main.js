c7aa8ee335dcfc0b82ed527f0af82b15a4d95db5
SykaBliet <aurele.guitard@gmail.com>
2021-06-27 00:13:39 +0200

    html css done

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*
+ Theme Name: Aurele Theme
+ Description: Aurele's Tic Tac To
+ Author: Aurele Guitard
+ Author URL:  https://github.com/hossamhilal/
+ Tags: Personal , Tech Test
+*/
+
+let board = [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+];
+
+let playerOne = "";
+let playerTwo = "";
